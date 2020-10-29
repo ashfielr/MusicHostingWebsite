067051c384a1f265459c5e4af96b8cf55d8ad0c7
Robert Ashfield <ashfielr@coventry.ac.uk>
2020-10-29 20:05:39 +0000

    Added time period for msg to be displayed for
    Msg was always visible upon page loading and not hidden later
    
    Msg will now only be visible for a given time period and then be hidden

--- public/main.js
+++ public/main.js
@@ -3,4 +3,11 @@
 
 window.addEventListener('DOMContentLoaded', () => {
 	console.log('DOMContentLoaded')
+
+	/* Show message for time period */
+	const msgDisplayTime = 1500
+	document.querySelector('.msg').hidden = false
+	window.setTimeout( () => {
+		document.querySelector('.msg').hidden = true
+	}, msgDisplayTime)
 })
